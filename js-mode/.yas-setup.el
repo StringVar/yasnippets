68a7f33cff04dbe597ae54224442c84411898212
Unknown <cameron.ag.white@gmail.com>
2019-02-23 22:15:25 -0700

    javascript mode + basic constructions

---
+++ js-mode/.yas-setup.el
@@ -0,0 +1,13 @@
+(defun inside-string-q ()
+  "Return non-nil if inside string, else nil.
+Result depends on syntax table's string quote character."
+  (let ((result (nth 3 (syntax-ppss))))
+    (message "%s" result)
+    result))
+
+(defun inside-comment-q ()
+  "Return non-nil if inside comment, else nil.
+Result depends on syntax table's comment character."
+  (let ((result (nth 4 (syntax-ppss))))
+    (message "%s" result)
+    result))
